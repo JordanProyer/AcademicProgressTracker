ec006d95fe94b9855c5d3cd5d3fb8faabecdc9fa
Jordan Proyer <jordan.proyer@hotmail.co.uk>
2017-11-08 01:43:08 +0000

    Initial code refactor

--- AcademicProgressTracker/Controllers/AccountController.cs
+++ AcademicProgressTracker/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using AcademicProgressTracker.Models;
+using AcademicProgressTracker.ViewModels;
 
 namespace AcademicProgressTracker.Controllers
 {
--- AcademicProgressTracker/Controllers/ManageController.cs
+++ AcademicProgressTracker/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using AcademicProgressTracker.Models;
+using AcademicProgressTracker.ViewModels;
 
 namespace AcademicProgressTracker.Controllers
 {
